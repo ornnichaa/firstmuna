52d18021d71912e09bf58c8dafc5c20d9d8fd886
ornnichaa <s6202041630024@email.kmutnb.ac.th>
2021-08-24 18:50:58 +0700

    เพิ่มapp kmutnb

---
+++ lib/index.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Index extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: ListView(
+          children: [
+            Image.asset(
+              "asset/image/logo.png",
+              width: size.width * 0.2,
+              height: size.height * 0.2,
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Text(
+              "Welcome to KMUTNB",
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.deepOrangeAccent,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Image(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1540151812223-c30b3fab58e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'),
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                  ),
+                  primary: Colors.amber[900],
+                  padding: EdgeInsets.all(20.0),
+                  shape: StadiumBorder()),
+              child: Text('LOGIN'),
+              onPressed: () {
+                print("LOGIN!!");
+              },
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                  ),
+                  primary: Colors.amber[900],
+                  padding: EdgeInsets.all(20.0),
+                  shape: StadiumBorder()),
+              child: Text('Sign Up'),
+              onPressed: () {
+                print("Sign Up");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
