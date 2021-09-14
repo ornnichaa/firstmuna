43a0843ab679ac2b8c54190bd6f7e7c76b767245
ornnichaa <s6202041630024@email.kmutnb.ac.th>
2021-09-14 18:55:49 +0700

    เพิ่มหน้าสมัครสมาชิก

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,3 +1,4 @@
+import 'package:fisrtmuna/config/constant.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -8,12 +9,143 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  var name, surname, email, password;
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Sign Up"),
+        ),
+        body: Form(
+          key: formkey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                txtName(),
+                txtSurname(),
+                txtEmail(),
+                txtPassword(),
+                btnSubmit(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget txtName() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Name:',
+          icon: Icon(Icons.approval),
+          hintText: 'Input your name',
+        ),
+        validator: (val) {
+          if (val!.length < 6) {
+            return 'กรุณากรอกข้อมูลมากกว่า 6 ตัวอักษร';
+          }
+        },
+        onSaved: (val) {
+          name = val;
+        },
+      ),
+    );
+  }
+
+  Widget txtSurname() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Surname:',
+          icon: Icon(Icons.android),
+          hintText: 'Input your surname',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'กรุณากรอกข้อมูล';
+          } else if (value.length < 3) {
+            return 'กรุณากรอกข้อมูลมากกว่า 3 ตัว';
+          }
+        },
+        onSaved: (value) {
+          surname = value!.trim();
+        },
+      ),
+    );
+  }
+
+  Widget txtEmail() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Email:',
+          icon: Icon(Icons.email),
+          hintText: 'Input your email',
+        ),
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (!(value!.contains('@'))) {
+            return 'กรุณากรอกข้อมูลตามรูปอีเมลด้วย';
+          } else if (!(value.contains('.'))) {
+            return 'กรุณากรอกข้อมูลตามรูปอีเมลด้วย';
+          }
+        },
+        onSaved: (value) {
+          email = value;
+        },
+      ),
+    );
+  }
+
+  Widget txtPassword() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+      child: TextFormField(
+        style: TextStyle(
+          color: sColor,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Password:',
+          icon: Icon(Icons.lock),
+          hintText: 'Input your password',
+        ),
+        obscureText: true,
+        onSaved: (value) {
+          password = value;
+        },
+      ),
+    );
+  }
+
+  Widget btnSubmit() {
+    return ElevatedButton(
+      child: Text('Submit'),
+      onPressed: () {
+        if (formkey.currentState!.validate()) {
+          formkey.currentState!.save();
+          print("ชื่อ: $name นามสกุล: $surname อีเมล: $email รหัส: $password");
+        }
+      },
     );
   }
 }
